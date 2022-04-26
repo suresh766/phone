8ee75d31e842d8820f084aff501da018bc035406
suresh766 <kumarsureshrock@gmail.com>
2022-04-27 00:03:45 +0530

    practice

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,12 @@
+package org.bank;
+
+public class AxisBank {
+	public void Deposit() {
+		System.out.println("deposit");
+   }
+	public static void main(String[] args) {
+		AxisBank x=new AxisBank();
+		x.Deposit();
+	}
+		
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,17 @@
+package org.bank;
+
+public class BankInfo{
+	public void Saving() {
+		System.out.println("savings");
+	}
+	public void Fixed() {
+		System.out.println("fixed");
+	}
+	public static void main(String[] args) {
+		BankInfo a=new BankInfo();
+		a.Saving();
+		a.Fixed();
+	}
+
+}
+     //single inheritance
